ecbfc363ec2fffe45971acfe185f8feb5be70b46
Renato-Rhuan <renato.rhuan@icloud.com>
2026-05-30 11:04:22 -0300

    Set API key placeholders and remove exposed secrets

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ async function cliqueiNoBotao() {
     let chave = "SUA_CHAVE_OPENWEATHER_AQUI"
 
 
-    let endereco = `https://api.openweathermap.org/data/3.0/weather?q=${cidade}&appid=${chave}&units=metric&lang=pt_br`
+    let endereco = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}&units=metric&lang=pt_br`
     // Precisa avisar o JavaScript que voce vai ate o servidor 
     // Traduzir a resposta do servidor / Json
 
